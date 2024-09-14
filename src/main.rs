96c4682a58608a15a8d9e4f0e3acf0b49e18c188
Soma Deme <demesoma@gmail.com>
2024-09-14 12:22:35 +0200

    Add 2 players, 1 ball, no collision

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use ggez::{Context, ContextBuilder, GameResult};
-use ggez::graphics::{self, Color, Mesh, DrawMode, Rect};
+use ggez::graphics::{self, Canvas, Color, DrawMode, Mesh, Rect};
 use ggez::event::{self, EventHandler};
+use ggez::glam::Vec2;
 
 // ===================== COLORS =====================
 pub const COL_BACKGROUND: Color = Color {
@@ -16,8 +17,16 @@ pub const COL_FOREGROUND: Color = Color {
     b: 0.8,
     a: 1.0,
 };
+
+pub const COL_BALL: Color = Color {
+    r: 0.8,
+    g: 0.8,
+    b: 1.0,
+    a: 1.0,
+};
 // --------------------- COLORS ---------------------
 
+// ===================== MAIN =====================
 fn main() {
     // Make a Context.
     let (mut ctx, event_loop) = ContextBuilder::new("my_game", "Cool Game Author")
@@ -32,39 +41,151 @@ fn main() {
     // Run!
     event::run(ctx, event_loop, my_game);
 }
+// --------------------- MAIN ---------------------
 
+// ===================== PLAYER =====================
+#[derive(Debug, Copy, Clone)]
+enum Side {
+    Left,
+    Right,
+}
+
+struct Player {
+    side: Side,
+    pos: Vec2,
+    size: Vec2,
+}
+
+impl Player {
+    fn new(side: &Side) -> Self {
+        Player {
+            side: *side,
+            pos: Vec2 {
+                // X position is based on side
+                x: match side {
+                    Side::Left => 40.0,
+                    Side::Right => 300.0,
+                },
+                y: 150.0
+            },
+            size: Vec2 {
+                x: 20.0,
+                y: 200.0
+            },
+        }
+    }
+
+    fn draw(&self, ctx: &mut Context, canvas: &mut Canvas) -> GameResult {
+
+        // Create rectangle
+        let rectangle = Mesh::new_rectangle(
+            ctx, 
+            DrawMode::fill(),
+            Rect::new(
+                -self.size.x / 2.0, 
+                -self.size.y / 2.0, 
+                self.size.x, 
+                self.size.y
+            ),
+            COL_FOREGROUND
+        )?;
+
+        // DEBUG
+        let center = Mesh::new_circle(ctx, DrawMode::fill(), Vec2 {x: 0.0, y: 0.0}, 5.0, 1.0, Color::RED)?;
+        canvas.draw(&center, self.pos);
+        // DEBUG END
+
+        // Draw rectangle
+        canvas.draw(&rectangle, self.pos);
+
+        Ok(())
+    }
+}
+// --------------------- PLAYER ---------------------
+
+// ===================== BALL =====================
+struct Ball {
+    pos: Vec2,
+    vel: Vec2,
+    size: f32,
+}
+
+impl Ball {
+    fn new() -> Self {
+        Ball {
+            pos: Vec2{ x: 300.0, y: 300.0 },
+            vel: Vec2{ x: 1.0, y: 0.0 },
+            size: 7.0,
+        }
+    }
+
+    fn update(&mut self) -> GameResult {
+        self.pos += self.vel;
+
+        Ok(())
+    }
+
+    fn draw(&self, ctx: &mut Context, canvas: &mut Canvas) -> GameResult {
+        // Create circle
+        let circle = Mesh::new_circle(
+            ctx, 
+            DrawMode::fill(), 
+            Vec2 {x: 0.0, y: 0.0}, 
+            self.size, 
+            1.0, 
+            COL_BALL
+        )?;
+
+        // Draw circle
+        canvas.draw(&circle, self.pos);
+
+        Ok(())
+    }
+}
+// --------------------- BALL ---------------------
+
+// ===================== GAME =====================
 struct MyGame {
-    // Your state here...
+    players: Vec<Player>,
+    ball: Ball,
 }
 
 impl MyGame {
     pub fn new(_ctx: &mut Context) -> MyGame {
         // Load/create resources such as images here.
         MyGame {
-            // ...
+            players: vec![Player::new(&Side::Left), Player::new(&Side::Right)],
+            ball: Ball::new(),
         }
     }
 }
 
 impl EventHandler for MyGame {
     fn update(&mut self, _ctx: &mut Context) -> GameResult {
-        // Update code here...
+        // Update player position
+
+        // Update ball position
+        self.ball.update()?;
+
+        // Update score
+        
         Ok(())
     }
 
     fn draw(&mut self, ctx: &mut Context) -> GameResult {
+        // Create canvas to draw on
         let mut canvas = graphics::Canvas::from_frame(ctx, COL_BACKGROUND);
-        // Draw code here...
 
-        // Draw rectangle
-        let rectangle: Mesh = Mesh::new_rectangle(ctx, DrawMode::fill(), Rect::new(-10.0, -100.0, 20.0, 200.0), COL_FOREGROUND)?;
-        canvas.draw(&rectangle, ggez::glam::Vec2::new(150.0, 380.0));
-        // Draw rectangle end
+        // Draw players
+        for player in &mut self.players {
+            player.draw(ctx, &mut canvas)?;
+        }
 
+        // Draw ball
+        self.ball.draw(ctx, &mut canvas)?;
 
-        // graphics::Drawable::draw(&self, canvas, param);
-        // canvas.draw(drawable, param);
-
+        // End draw
         canvas.finish(ctx)
     }
 }
+// --------------------- GAME ---------------------
